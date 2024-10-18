57ea9a3c11dd877e694ab0a21d78e4e2ac7eacd5
Pedro <pedroabernardes11@gmail.com>
2024-10-18 20:40:00 -0300

    feat: add pages scope

---
+++ src/pages/losses/losses.tsx
@@ -0,0 +1,3 @@
+export function Losses() {
+  return <div>Losses</div>
+}
--- src/router.tsx
+++ src/router.tsx
@@ -1,14 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
 import { DefaultLayout } from '@/layouts/default-layout'
 import { ManagersTasks } from './pages/managers-tasks/managers-tasks'
+import { Invoicing } from './pages/invoincing/invoicing'
+import { Losses } from './pages/losses/losses'
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
-        <Route path="invoincing" element={<ManagersTasks />} />
+        <Route path="invoincing" element={<Invoicing />} />
         <Route path="managers-tasks" element={<ManagersTasks />} />
-        <Route path="losses" element={<ManagersTasks />} />
+        <Route path="losses" element={<Losses />} />
       </Route>
     </Routes>
   )
