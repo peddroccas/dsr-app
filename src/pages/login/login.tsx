b5cf5c6c8faec5b9e90ff170b8654a1b513a79fa
Pedro <pedroabernardes11@gmail.com>
2024-10-21 11:37:25 -0300

    feat: login page

---
+++ src/pages/login/login.tsx
@@ -0,0 +1,25 @@
+import SRLogo from '@/assets/sao-rafael-logo.svg'
+import { TextField } from '@mui/material'
+
+export function Login() {
+  return (
+    <div className="bg-venice-blue-950 h-screen">
+      <header className="relative flex justify-center items-center bg-slate-50 h-24">
+        <img className="absolute max-h-48" src={SRLogo} alt="sr-logo" />
+      </header>
+      <main className="flex justify-center m-16 ">
+        <form className="flex flex-col gap-2 items-center bg-slate-50 w-fit p-4 rounded-xl ">
+          <h1 className="text-ignara font-semibold text-xl">ENTRAR</h1>
+          <TextField
+            label="Email"
+            color="warning"
+            slotProps={{
+              input: { className: 'rounded-2xl  border-ignara-500' },
+              formHelperText: { className: 'border-ignara-500 border' },
+            }}
+          />
+        </form>
+      </main>
+    </div>
+  )
+}
--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,15 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import { DefaultLayout } from '@/layouts/default-layout'
 import { ManagersTasks } from './pages/managers-tasks/managers-tasks'
 import { Invoicing } from './pages/invoincing/invoicing'
 import { Losses } from './pages/losses/losses'
+import { Login } from './pages/login/login'
 
 export function Router() {
   return (
     <Routes>
+      <Route path="" element={<Navigate to={'/login'} />} />
+      <Route path="/login" element={<Login />} />
       <Route path="/" element={<DefaultLayout />}>
         <Route path="invoincing" element={<Invoicing />} />
         <Route path="managers-tasks" element={<ManagersTasks />} />
