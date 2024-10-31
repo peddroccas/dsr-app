8d7bbeb277563ed57b7bbee2a6bf0932c623a134
Pedro Antunes <pedroabernardes11@gmail.com>
2024-10-31 17:15:54 -0300

    refactor: resize drawer

--- src/pages/managers-tasks/components/task-tabs.tsx
+++ src/pages/managers-tasks/components/task-tabs.tsx
@@ -7,27 +7,27 @@ export default function TaskTabs() {
       defaultValue="Tarefas"
       className="w-full h-full flex flex-col sm:flex-row sm:items-start gap-8 justify-center items-center"
     >
-      <TabsList className="grid sm:grid-rows-6 sm:grid-cols-1 grid-cols-3 bg-inherit h-full gap-4 text-venice-blue-900 bg-slate-50 p-2 rounded">
+      <TabsList className="grid sm:grid-rows-4  grid-cols-1 bg-inherit h-full gap-4 text-venice-blue-900 bg-slate-50 p-2 rounded">
+        <TabsTrigger value="Pendentes de Aprovação">
+          Pendentes de Aprovação
+        </TabsTrigger>
+        <TabsTrigger value="Não Realizadas">Não Realizadas</TabsTrigger>
+        <TabsTrigger value="Aprovadas">Aprovadas</TabsTrigger>
         <TabsTrigger value="Tarefas">Tarefas</TabsTrigger>
-        <TabsTrigger value="Todas">Todas</TabsTrigger>
-        <TabsTrigger value="São Rafael">São Rafael</TabsTrigger>
-        <TabsTrigger value="Estrela">Estrela</TabsTrigger>
-        <TabsTrigger value="Antunes">Antunes</TabsTrigger>
-        <TabsTrigger value="São Rafael 2">São Rafael 2</TabsTrigger>
       </TabsList>
       <TabsContent value="Tarefas" className="flex-1">
         <TaskCards />
       </TabsContent>
-      <TabsContent value="Todas" className="flex-1">
+      <TabsContent value="Pendentes de Aprovação" className="flex-1">
         <div className="w-full">pedro</div>
       </TabsContent>
       <TabsContent value="São Rafael" className="flex-1">
         <div className="w-full">pedro</div>
       </TabsContent>
-      <TabsContent value="Estrela" className="flex-1">
+      <TabsContent value="Não Realizadas" className="flex-1">
         <div className="w-full">pedro</div>
       </TabsContent>
-      <TabsContent value="Antunes" className="flex-1">
+      <TabsContent value="Aprovadas" className="flex-1">
         <div className="w-full">pedro</div>
       </TabsContent>
       <TabsContent value="São Rafael 2" className="flex-1">
