be29a01c4cb6f6c5673684eab42a372c3c070d37
Pedro Antunes <pedroabernardes11@gmail.com>
2024-10-31 16:55:41 -0300

    feat: font familys

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -10,10 +10,10 @@ export default function Card({ key, children, title }: CardProps) {
   return (
     <UICard
       key={key}
-      className="hover:scale-105 transition-transform duration-300 cursor-pointer shadow-black rounded-lg overflow-hidden"
+      className="hover:scale-105 transition-transform duration-300 cursor-pointer shadow-black rounded-lg overflow-hidden font-roboto"
     >
       <CardHeader className="bg-ignara p-4 text-slate-50">
-        <CardTitle className="text-lg font-semibold">{title}</CardTitle>
+        <CardTitle className="text-xl font-semibold">{title}</CardTitle>
       </CardHeader>
       <CardContent className="p-4 space-y-2">{children}</CardContent>
     </UICard>
