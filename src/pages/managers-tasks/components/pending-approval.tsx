2fe576d05f4b1012d4c8be28fd31d8d12fe2f405
Pedro Antunes <pedroabernardes11@gmail.com>
2024-10-31 17:39:13 -0300

    refactor: delete unnecessary component

---
+++ src/pages/managers-tasks/components/pending-approval.tsx
@@ -0,0 +1,7 @@
+export default function PendingApproval() {
+  return (
+    <div className="grid h-full sm:grid-cols-2 md:grid-cols-3 2xl:grid-cols-4 gap-8 bg-venice-blue-900 p-8 rounded-md">
+      pending-approval
+    </div>
+  )
+}
