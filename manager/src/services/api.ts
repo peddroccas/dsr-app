28e441dac60ef2f465ca19b636f1fd8c14305a0c
Pedro Antunes <pedroabernardes11@gmail.com>
2024-11-04 15:08:47 -0300

    fix: ci test bug

--- manager/src/services/api.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  headers: {
-    Accept: 'application/json, text/plain, */*',
-    'Content-Type': 'application/json',
-  },
-})
