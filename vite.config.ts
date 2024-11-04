099a8544daedd2bc8a239a24566ac3784efd8918
Pedro Antunes <pedroabernardes11@gmail.com>
2024-11-04 15:12:08 -0300

    chore: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,12 @@
-import path from "node:path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import path from 'node:path'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-	base: "/admin",
-	build: {
-		outDir: "dist", // Saída para o diretório dist
-	},
-	plugins: [react()],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
-});
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
